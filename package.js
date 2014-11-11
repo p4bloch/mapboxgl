6eb6a00d28b1fdee835edf7539cb8f222f5211c5
p4bloch <pchiappetti@gmail.com>
2014-11-11 12:10:12 -0300

    Initial Commit
    Start package

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  name: 'p4bloch:mapboxgl',
+  summary: 'Wrapper for MapboxGL JS Library',
+  version: '0.4.0',
+  git: 'https://github.com/p4bloch/mapboxgl.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.0');
+  api.addFiles('css/mapbox-gl.v0.4.0.css', 'client');
+  api.addFiles('js/mapbox-gl.v0.4.0.js', 'client');
+});
